f1612a74f4c9032740ad88f07af36728f8025f46
Jeongwon Choi <chlwjddnjs17@naver.com>
2023-08-10 12:02:11 +0900

    인프런 자바 알고리즘 / 재귀함수(스택 프레임)

---
+++ src/pkg1/Main.java
@@ -0,0 +1,19 @@
+package pkg1;
+
+public class Main {
+	// 재귀함수(스택프레임)
+	public void DFS(int n) {
+		if(n==0) return;
+		else {
+			DFS(n-1);
+			System.out.print(n + " ");
+		}
+	}
+	
+	public static void main(String[] args) {
+	 
+		Main T = new Main();
+		T.DFS(3);
+	}
+
+}
